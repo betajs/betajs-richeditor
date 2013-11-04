9da738d9a793e0d3d918b2da3a2b322c2f028d16
Oliver Friedmann <oliverfriedmann@Oliver-Friedmanns-MacBook-Air.local>
2013-11-04 17:37:50 -0500

    Update

--- src/views/simple_rich_editor_view/view.js
+++ src/views/simple_rich_editor_view/view.js
@@ -1,44 +1,112 @@
-BetaJS.Views.View.extend("BetaJS.Views.SimpleRichEditorView", {
+BetaJS.Browser.Dom = {
 	
+	traverseNext: function (node, skip_children) {
+		if ("get" in node)
+			node = node.get(0);
+		if (node.firstChild && !skip_children)
+			return BetaJS.$(node.firstChild);
+		if (!node.parentNode)
+			return null;
+		if (node.nextSibling)
+			return BetaJS.$(node.nextSibling);
+		return this.traverseNext(node.parentNode, true);
+	},
+	
+	selectionStartNode : function() {
+		if (window.getSelection)
+			return BetaJS.$(window.getSelection().getRangeAt(0).startContainer);
+		else if (document.selection)
+			return BetaJS.$(document.selection.createRange().startContainer);
+		return null;
+	},
+	
+	selectedHtml : function() {
+		if (window.getSelection)
+			return window.getSelection().toString();
+		else if (document.selection)
+			return document.selection.createRange().htmlText;
+	},
+	
+	selectionAncestor : function() {
+		if (window.getSelection)
+			return BetaJS.$(window.getSelection().getRangeAt(0).commonAncestorContainer);
+		else if (document.selection)
+			return BetaJS.$(document.selection.createRange().parentElement());
+		return null;
+	},
+	
+	selectionStart : function() {
+		if (window.getSelection)
+			return BetaJS.$(window.getSelection().getRangeAt(0).startContainer);
+		else if (document.selection)
+			return BetaJS.$(document.selection.createRange().startContainer);
+		return null;
+	},
+
+	selectionEnd : function() {
+		if (window.getSelection)
+			return BetaJS.$(window.getSelection().getRangeAt(0).endContainer);
+		else if (document.selection)
+			return BetaJS.$(document.selection.createRange().endContainer);
+		return null;
+	},
+
+	selectionNodes: function () {
+		var result = [];
+		var start = this.selectionStart();
+		var end = this.selectionEnd();
+		result.push(start);
+		var current = start;
+		while (current.get(0) != end.get(0)) {
+			current = this.traverseNext(current);
+			result.push(current);
+		}
+		return result;
+	}
+	
+};
+
+BetaJS.Views.View.extend("BetaJS.Views.SimpleRichEditorView", {
+
 	_templates : {
 		"default" : BetaJS.Templates.Cached["simple-rich-editor-view-template"]
 	},
-	
-	constructor: function (options) {
+
+	constructor : function(options) {
 		this._inherited(BetaJS.Views.SimpleRichEditorView, "constructor", options);
 		this._setOptionProperty(options, "content", "");
+		this._selector = "[data-view-id='" + this.cid() + "']";
 		this.actions.__context = this;
 		this.selection.__context = this;
 		this.caret.__context = this;
 	},
-	
-	_events: function () {
+
+	_events : function() {
 		return this._inherited(BetaJS.Views.SimpleRichEditorView, "_events").concat([{
-			"blur [data-selector='inner']": "__change",
-			"keyup [data-selector='inner']": "__change",
-			"paste [data-selector='inner']": "__change"
+			"blur [data-selector='inner']" : "__change",
+			"keyup [data-selector='inner']" : "__change",
+			"paste [data-selector='inner']" : "__change"
 		}]);
 	},
-	
-	__change: function () {
+
+	__change : function() {
 		this.set("content", this.$("[data-selector='inner']").html());
 	},
-	
-	_render: function () {
+
+	_render : function() {
 		this._inherited(BetaJS.Views.SimpleRichEditorView, "_render");
 		this._editor = this.$("[data-selector='inner']");
 	},
-	
-	actions: {
-		__context: null,
-		
-		hasParentElement: function (element) {
-			var selection = this.__context.selection;
-			var caret = this.__context.caret;
-			return selection.isSelected() ? selection.hasParentElement(element) : caret.hasParentElement(element);
+
+	actions : {
+		__context : null,
+
+		hasParentElement : function(element) {
+			var access = this.__context.selection.isSelected() ? this.__context.selection : this.__context.caret;
+			return this.__context.selection.isSelected() ? access.hasParentElement(element) : access.hasParentElement(element);
 		},
-		
-		setParentElement: function (element, value) {
+
+		setParentElement : function(element, value) {
 			var has = this.hasParentElement(element);
 			if (BetaJS.Types.is_undefined(value))
 				value = !has;
@@ -50,59 +118,82 @@ BetaJS.Views.View.extend("BetaJS.Views.SimpleRichEditorView", {
 			else
 				access.addParentElement(element);
 		},
-		
-		isBold: function () {
+
+		isBold : function() {
 			return this.hasParentElement("strong");
 		},
-		
-		bold: function (value) {
+
+		bold : function(value) {
 			this.setParentElement("strong", value);
 		},
 
-		isItalic: function () {
+		isItalic : function() {
 			return this.hasParentElement("i");
 		},
-		
-		italic: function (value) {
+
+		italic : function(value) {
 			this.setParentElement("i", value);
 		}
 	},
-	
-	selection: {
-		__context: null,
+
+	selection : {
+		__context : null,
+
+		isSelected : function() {
+			return BetaJS.Browser.Dom.selectedHtml() != "";
+		},
 		
-		isSelected: function () {
+		ancestor: function () {
+			return BetaJS.Browser.Dom.selectionAncestor();
+		},
+		
+		selection: function () {
+			return BetaJS.Browser.Dom.selectionNodes();
+		},
+		
+		selectionLeaves: function () {
+			var sel = this.selection();
+			return BetaJS.Objs.filter(this.selection(), function (node) {
+				return node.children().length == 0;
+			});
+		},
+
+		hasParentElement : function(element) {
+			if (this.ancestor().parents(this.__context._selector + " " + element).length > 0)
+				return true;
+			return BetaJS.Objs.all(this.selectionLeaves(), function (node) {
+				return (node.parents(this.__context._selector + " " + element).length > 0) ||
+				       (BetaJS.Types.is_defined(node.get(0).tagName) && node.get(0).tagName.toLowerCase() == element.toLowerCase());
+			}, this);
+		},
+
+		addParentElement : function(element) {
 			// TODO
-			return true;
 		},
-		
-		hasParentElement: function (element) {
-			return BetaJS.Browser.Dom.hasParentElementsTag(element, BetaJS.Browser.Dom.parentElementBySelection(), this.__context._editor.get(0));
-		},
-		
-		addParentElement: function (element) {
-			// TODO			
-		},
-		
-		removeParentElement: function (element) {
-			// TODO			
+
+		removeParentElement : function(element) {
+			// TODO
 		}
 	},
-	
-	caret: {
-		__context: null,
-		
-		hasParentElement: function (element) {
-			return BetaJS.Browser.Dom.hasParentElementsTag(element, BetaJS.Browser.Dom.parentElementBySelection(), this.__context._editor);
+
+	caret : {
+		__context : null,
+
+		node : function() {
+			return BetaJS.Browser.Dom.selectionStartNode();
 		},
-		
-		addParentElement: function (element) {
-			// TODO			
+
+		hasParentElement : function(element) {
+			return this.node().parents(this.__context._selector + " " + element).length > 0;
 		},
-		
-		removeParentElement: function (element) {
-			// TODO			
+
+		addParentElement : function(element) {
+			// TODO
+		},
+
+		removeParentElement : function(element) {
+			// TODO
 		}
 	}
-	
-});
+
+}); 
