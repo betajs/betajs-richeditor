973607e7d3c26811ba49a262be3e664b9d693f62
Oliver Friedmann <oliverfriedmann@Oliver-Friedmanns-MacBook-Air.local>
2013-11-04 14:35:40 -0500

    Update

--- dist/betajs-richeditor.js
+++ dist/betajs-richeditor.js
@@ -1,5 +1,5 @@
 /*!
-  betajs-codemirror - v0.0.1 - 2013-11-03
+  betajs-codemirror - v0.0.1 - 2013-11-04
   Copyright (c) Victor Lingenthal
   MIT Software License.
 */
@@ -17,6 +17,9 @@ BetaJS.Views.View.extend("BetaJS.Views.SimpleRichEditorView", {
 	constructor: function (options) {
 		this._inherited(BetaJS.Views.SimpleRichEditorView, "constructor", options);
 		this._setOptionProperty(options, "content", "");
+		this.actions.__context = this;
+		this.selection.__context = this;
+		this.caret.__context = this;
 	},
 	
 	_events: function () {
@@ -29,7 +32,88 @@ BetaJS.Views.View.extend("BetaJS.Views.SimpleRichEditorView", {
 	
 	__change: function () {
 		this.set("content", this.$("[data-selector='inner']").html());
-	}	
+	},
+	
+	_render: function () {
+		this._inherited(BetaJS.Views.SimpleRichEditorView, "_render");
+		this._editor = this.$("[data-selector='inner']");
+	},
+	
+	actions: {
+		__context: null,
+		
+		hasParentElement: function (element) {
+			var selection = this.__context.selection;
+			var caret = this.__context.caret;
+			return selection.isSelected() ? selection.hasParentElement(element) : caret.hasParentElement(element);
+		},
+		
+		setParentElement: function (element, value) {
+			var has = this.hasParentElement(element);
+			if (BetaJS.Types.is_undefined(value))
+				value = !has;
+			if (value == has)
+				return;
+			var access = this.__context.selection.isSelected() ? this.__context.selection : this.__context.caret;
+			if (has)
+				access.removeParentElement(element);
+			else
+				access.addParentElement(element);
+		},
+		
+		isBold: function () {
+			return this.hasParentElement("strong");
+		},
+		
+		bold: function (value) {
+			this.setParentElement("strong", value);
+		},
+
+		isItalic: function () {
+			return this.hasParentElement("i");
+		},
+		
+		italic: function (value) {
+			this.setParentElement("i", value);
+		}
+	},
+	
+	selection: {
+		__context: null,
+		
+		isSelected: function () {
+			// TODO
+			return true;
+		},
+		
+		hasParentElement: function (element) {
+			return BetaJS.Browser.Dom.hasParentElementsTag(element, BetaJS.Browser.Dom.parentElementBySelection(), this.__context._editor.get(0));
+		},
+		
+		addParentElement: function (element) {
+			// TODO			
+		},
+		
+		removeParentElement: function (element) {
+			// TODO			
+		}
+	},
+	
+	caret: {
+		__context: null,
+		
+		hasParentElement: function (element) {
+			return BetaJS.Browser.Dom.hasParentElementsTag(element, BetaJS.Browser.Dom.parentElementBySelection(), this.__context._editor);
+		},
+		
+		addParentElement: function (element) {
+			// TODO			
+		},
+		
+		removeParentElement: function (element) {
+			// TODO			
+		}
+	}
 	
 });
 BetaJS.Views.ListContainerView.extend("BetaJS.Views.RichEditorView", {
@@ -92,11 +176,12 @@ BetaJS.Views.ListContainerView.extend("BetaJS.Views.RichEditorView", {
 				parent: "toolbar",
 				options: {
 					label: "B",
-					children_classes: "bold"
+					children_classes: "bold",
+					hotkey: "ctrl+b",
 				},
 				events: {
 					"click": function () {
-						alert("Bold");
+						alert("Bold? " + this.domain.ns.editor_view.actions.isBold());
 					}
 				},
 			},
@@ -106,11 +191,12 @@ BetaJS.Views.ListContainerView.extend("BetaJS.Views.RichEditorView", {
 				parent: "toolbar",
 				options: {
 					label: "I",
-					children_classes: "italic"
+					children_classes: "italic",
+					hotkey: "ctrl+i"
 				},
 				events: {
 					"click": function () {
-						alert("Italic");
+						alert("Italic? " + this.domain.ns.editor_view.actions.isItalic());
 					}
 				},
 			},
@@ -193,9 +279,11 @@ BetaJS.Views.ListContainerView.extend("BetaJS.Views.RichEditorView", {
 			editor_view: {
 				type: "BetaJS.Views.SimpleRichEditorView",
 				el: ".editbox",
-				options:{
-					children_classes: "textareadiv",
-					content: this.binding("content")
+				options: function (page) {
+					return {
+						children_classes: "textareadiv",
+						content: page.binding("content")
+					};
 				}
 			}
 
